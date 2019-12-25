6d4e26b992b9620ec23177a579af3f78f8fe7bfa
Anton <deripas@yandex.ru>
2019-12-25 22:53:23 +0300

    fix structure size test

--- src/test/java/ru/lcard/ltr/StructUtil.java
+++ src/test/java/ru/lcard/ltr/StructUtil.java
@@ -1,34 +1,20 @@
 package ru.lcard.ltr;
 
-import jnr.ffi.NativeType;
 import jnr.ffi.Struct;
-import jnr.ffi.provider.FFIProvider;
 import lombok.experimental.UtilityClass;
 import org.junit.Assert;
 
 @UtilityClass
 public class StructUtil {
 
-    /**
-     * Can be either 4 or 8 bytes in size, depending on the platform.
-     */
-    private static final int ADDRESS_SIZE = FFIProvider.getSystemProvider().getRuntime()
-            .findType(NativeType.ADDRESS)
-            .size();
-
-    /**
-     * Can be either 3 or 7 bytes in size, depending on the platform.
-     */
-    private static final int ADDRESS_MASK = ADDRESS_SIZE - 1;
-
     public static void assertSize(int expectedSize, Struct struct) {
-        int actualSize = Struct.size(struct);
-        if (expectedSize == actualSize) return; // equals, OK
+        int size = Struct.size(struct);
+        if (expectedSize == size) return; // equals, OK
 
         // пробуем применить выравнивание по размерности адреса
-        int mod = actualSize & ADDRESS_MASK;
-        int fixSize = mod == 0 ? actualSize : actualSize - mod + ADDRESS_SIZE;
-        if (expectedSize == fixSize) return; // equals, OK
+        int addressSize = struct.getRuntime().addressSize();
+        int correctSize = size + addressSize - (size & (addressSize - 1));
+        if (expectedSize == correctSize) return; // equals, OK
 
         Assert.fail("bad structure");
     }
